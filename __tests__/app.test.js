b02f3f0b65f74e1bdf86057c9caaaf106f2d72aa
Yussuf Shariff <yussufshariff@outlook.com>
2023-01-19 19:11:33 +0000

    Changed to literal object

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -127,22 +127,23 @@ describe("NCNews API testing", () => {
     });
   });
   describe("GET/api/articles/:article_id", () => {
-    test("return correct article when passed an article id ", () => {
+    test.only("return correct article when passed an article id ", () => {
       return request(app)
-        .get("/api/articles/2")
+        .get("/api/articles/1")
         .expect(200)
         .then(({ body }) => {
           expect(body).toEqual({
             articles: {
-              author: expect.any(String),
-              title: expect.any(String),
-              article_id: expect.any(Number),
-              body: expect.any(String),
-              topic: expect.any(String),
-              created_at: expect.any(String),
-              votes: expect.any(Number),
-              article_img_url: expect.any(String),
-              comment_count: expect.any(String),
+              article_id: 1,
+              title: "Living in the shadow of a great man",
+              topic: "mitch",
+              author: "butter_bridge",
+              body: "I find this existence challenging",
+              created_at: "2020-07-09T20:11:00.000Z",
+              votes: 100,
+              article_img_url:
+                "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+              comment_count: "11",
             },
           });
           expect(Object.keys(body.articles)).toHaveLength(9);
